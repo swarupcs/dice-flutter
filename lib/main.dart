4d4d42e932d8b7290f1cbddb663763b77c4b1af6
SwarupDas <78835981+swarupcs@users.noreply.github.com>
2023-07-08 08:14:08 +0530

    Individual Button Works

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
           title: Text('Dicee'),
+
           backgroundColor: Colors.red,
         ),
         body: DicePage(),
@@ -15,9 +18,55 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+
+
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      leftDiceNumber = Random().nextInt(6)+1;
+                    });
+                    print("Left Button Pressed $leftDiceNumber");
+              },
+                  child: Image.asset('images/dice$leftDiceNumber.png')),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      rightDiceNumber = Random().nextInt(6)+1;
+                    });
+                    print("Right Button Pressed $rightDiceNumber");
+                  },
+                  child: Image.asset('images/dice$rightDiceNumber.png')),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
+
